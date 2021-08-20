a8923050f0dd4bef75bfa3f7d49da871f929e0ce
MiketoCoding <le.michael.v@gmail.com>
2021-08-20 17:36:55 -0400

    Add gifs and links to projects
    Gifs instead of static images and links to projects added to portfolio.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -54,6 +54,6 @@ window.addEventListener('DOMContentLoaded', event => {
 
   // Activate SimpleLightbox plugin for portfolio items
   new SimpleLightbox({
-    elements: '#portfolio a.portfolio-box'
+    // elements: '#portfolio a.portfolio-box'
   })
 })
